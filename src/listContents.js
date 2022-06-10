4b4a83925ea803aa289d5c37015b1b36ca192b1d
anijit <anijitsau@gmail.com>
2022-06-10 19:21:38 +0530

    IAM roles per functions added

--- src/listContents.js
+++ src/listContents.js
@@ -1,6 +1,20 @@
+// AWS SDK dependencies
+import { ListObjectsCommand } from "@aws-sdk/client-s3";
+
+// local dependencies
 import { sendResponse } from "./helpers/sendResponse.js";
+import { s3Ops } from "./libs/s3Client.js";
 
 export async function listContentsHandler(event) {
-  const res = { message: "API endpoint reached" };
-  return sendResponse(process.env.SUCCESS_CODE, res);
+  try {
+    const params = { Bucket: process.env.FILE_UPLOAD_BUCKET };
+    const { Contents, IsTruncated, Name, Marker } = await s3Ops.send(
+      new ListObjectsCommand(params)
+    );
+    const res = { Contents, IsTruncated, Name, Marker };
+    return sendResponse(process.env.SUCCESS_CODE, res);
+  } catch (error) {
+    const res = { message: "Unable to find Bucket Contents", error };
+    return sendResponse(process.env.ERROR_CODE, res);
+  }
 }
